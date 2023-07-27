80d8ffca340d9a37e1e011bc157d315c29f261b8
AliHamzaraja <alihamzarajpoot88@gmail.com>
2023-07-27 16:54:24 +0500

    bloc

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,19 +10,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: BlocBuilder<InternetBloc, InternetState>(
-          builder: (context, state) {
-            if (state is InternetrGainedState) {
-              return Text('Internet Connected');
-            } else if (state is InternetLostState) {
-              return Text('Internet Lost');
-            } else {
-              return Text('Loading...');
-            }
-          },
-        ),
+        body: Center(
+      child: BlocConsumer<InternetBloc, InternetState>(
+        listener: (context, state) {
+          if (state is InternetrGainedState) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+                'Internet Connected',
+              ),
+              backgroundColor: Colors.green,
+            ));
+            
+          }
+          else if(state is InternetLostState){
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Internet Lost'), backgroundColor: Colors.red,)
+            )
+          }
+        },
+        builder: (context, state) {
+          if (state is InternetrGainedState) {
+            return Text('Internet Connected');
+          } else if (state is InternetLostState) {
+            return Text('Internet Lost');
+          } else {
+            return Text('Loading...');
+          }
+        },
       ),
-    );
+    )
+
+        // body: Center(
+        //   child: BlocBuilder<InternetBloc, InternetState>(
+        //     builder: (context, state) {
+        //       if (state is InternetrGainedState) {
+        //         return Text('Internet Connected');
+        //       } else if (state is InternetLostState) {
+        //         return Text('Internet Lost');
+        //       } else {
+        //         return Text('Loading...');
+        //       }
+        //     },
+        //   ),
+        // ),
+        );
   }
 }
